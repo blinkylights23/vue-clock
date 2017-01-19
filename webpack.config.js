40c2afca3a0589a120fac4e5d6445a5f07567c10
Paul Smith <paul@blinkylights.org>
2017-01-19 05:36:19 -0500

    Babel, webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, '/src'),
+  entry: {
+    'vue-clock': './index.js'
+  },
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[name].min.js'
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.common.js'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }]
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.vue$/, loader: 'vue-loader' }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/build')
+  }
+};
